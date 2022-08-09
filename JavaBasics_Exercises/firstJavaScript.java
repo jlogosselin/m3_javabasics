6585a1795dc1ea0fc7bfcfec4598212211a857cc
jlogosselin <jlgosselingithub@gmail.com>
2022-08-09 13:42:28 +0100

    very first Java script create

---
+++ JavaBasics_Exercises/firstJavaScript.java
@@ -0,0 +1,12 @@
+package JavaBasics_Exercises;
+
+public class firstJavaScript {
+    public static void main(String[] args) {
+
+        int v1 = 56;
+        int v2 = 533;
+        int result = v1 + v2;
+
+        System.out.println("Result = " + result);
+    }
+}
