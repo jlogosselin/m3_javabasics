967d66dd0aefad7cb19d0e9362be02e55319a20d
jlogosselin <jlgosselingithub@gmail.com>
2022-08-15 11:57:09 +0100

    final code amendment

--- JavaBasicsAssessment/RockPaperScissors.java
+++ JavaBasicsAssessment/RockPaperScissors.java
@@ -5,18 +5,16 @@ import java.util.Scanner;
 public class RockPaperScissors {
 
     public static void main(String[] args) {
-        Scanner userInput = new Scanner(System.in);
         String playAgain = "";
         do{
             int playerWins = 0;
             int computerWins = 0;
             int draws = 0;
-            int numberOfRounds = chooseNumberRounds();
+            int numberOfRounds = enterInteger(1);
             introMessage();
 
             for(int i=0; i<numberOfRounds; i++) {
 
-                System.out.println("Play (1) for Rock, (2) for Paper, (3) for Scissors");
                 int playerMove = playerMove();
                 int computerMove = computerMove();
                 System.out.println("You played " + showPlay(playerMove) + ", computer played " + showPlay(computerMove));
@@ -36,15 +34,36 @@ public class RockPaperScissors {
         finalMessage();
     }
 
-    public static int chooseNumberRounds(){
+    public static int enterInteger(int x){
         Scanner userInput = new Scanner(System.in);
+        String n;
+        boolean r1, r2=false;
+        do{
+            if(x == 1) enterNumberRoundsMessage();
+            else if (x == 2) enterPlayerMove();
+            n = userInput.nextLine();
+            r1 = isNumeric(n);
+            if(r1){
+                if(Integer.parseInt(n) >= 1 && Integer.parseInt(n) <= 10){
+                    r1 = true;
+                    r2 = true;
+                }
+            }
+        }while(r1 == false || r2 == false);
+        return Integer.parseInt(n);
+    }
+
+    public static void enterNumberRoundsMessage(){
         System.out.println("Choose a number of rounds, between 1 and 10: ");
-        int numberOfRounds = Integer.parseInt(userInput.nextLine());
-        while(numberOfRounds < 1 || numberOfRounds > 10){
-            System.out.println("Sorry, incorrect input. Choose a number of rounds, between 1 and 10: ");
-            numberOfRounds = Integer.parseInt(userInput.nextLine());
-        }
-        return numberOfRounds;
+    }
+
+    public static void enterPlayerMove(){
+        System.out.println("Play (1) for Rock, (2) for Paper, (3) for Scissors");
+    }
+
+    public static boolean isNumeric(String s) {
+        boolean result = s.matches("-?\\d+");
+        return result;
     }
 
     public static void introMessage(){
@@ -52,24 +71,16 @@ public class RockPaperScissors {
     }
 
     public static int playerMove(){
-        Scanner userInput = new Scanner(System.in);
-        int playerMove = Integer.parseInt(userInput.nextLine());
-        while(playerMove < 1 || playerMove > 3){
-            System.out.println("Sorry, incorrect input! Please play (1) for Rock, (2) for Paper, (3) for Scissors");
-            playerMove = Integer.parseInt(userInput.nextLine());
-        }
-        return playerMove;
+        return enterInteger(2);
     }
 
     public static int computerMove(){
-        Scanner userInput = new Scanner(System.in);
         int min = 1, max = 3;
         int computerMove = (int)(Math.random()*(max-min+1)+min);
         return computerMove;
     }
 
     public static void settingScores(int[] scoreContainer, int i) {
-
         if (scoreContainer[0] == 1) {
             if (scoreContainer[1] == 1) scoreContainer[2] += roundIsDraw(i);
             else if (scoreContainer[1] == 2) scoreContainer[3] += computerWinsRound(i);
